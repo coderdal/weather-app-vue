0dde9886c8a5e229589461fdcd4c756fb4c865ea
coderdal <agircoder@gmail.com>
2022-07-06 12:55:22 +0300

    Setup & Header

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/appHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #f2f2f2;
+  color: #2c3e50;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+}
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <a href="#"><h1>HoWeather</h1></a>
+  </header>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid rgb(224, 224, 224);
+}
+
+header a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
